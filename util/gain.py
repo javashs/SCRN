deb2c0cb6b6920641c04f182f24536ac096d6097
javashs <91184827+javashs@users.noreply.github.com>
2023-11-14 19:10:51 +0800

    Update gain.py

--- util/gain.py
+++ util/gain.py
@@ -21,47 +21,47 @@ def gain(data, dt, option1, parameters, option2):
       OUT  dout(nt,nx): traces after application of gain function
     '''
 
-    nt, nx = data.shape  # nt采样点，nx道数
-    dout = np.zeros(data.shape)  # 初始化和data.shape相同的维度
+    nt, nx = data.shape  
+    dout = np.zeros(data.shape)  
 
-    if option1 == 'time':  # 通过时间
-        a = parameters[0]  # 第一个参数
-        b = parameters[1]  # 第二个参数
-        t = [x*dt for x in range(nt)]  # 每个采样点乘采样间隔
-        tgain = [(x**a)*math.exp(x*b) for x in t]  # 增益函数
-
-        for k in range(nx):
-            dout[:, k] = data[:, k]*tgain  # 原始数据乘增益函数
-
-    elif option1 == 'agc':  # 通过窗口
-        L = parameters/dt+1  # 得到一个L
-        L = np.floor(L/2)   # L向下除2取整
-        h = triang(2*L+1)  # 变成窗口的向量
-        shaped_h = h.reshape(len(h), 1)  # 转成矩阵
+    if option1 == 'time':  
+        a = parameters[0]  
+        b = parameters[1]  
+        t = [x*dt for x in range(nt)]  
+        tgain = [(x**a)*math.exp(x*b) for x in t]  
 
         for k in range(nx):
-            aux = data[:, k]  # 获取第k道
-            e = aux**2  # 取平方
-            shaped_e = e.reshape(len(e), 1)  # 转成矩阵
+            dout[:, k] = data[:, k]*tgain  
+
+    elif option1 == 'agc':  
+        L = parameters/dt+1  
+        L = np.floor(L/2)  
+        h = triang(2*L+1)  
+        shaped_h = h.reshape(len(h), 1)  
+
+        for k in range(nx):
+            aux = data[:, k]  
+            e = aux**2  
+            shaped_e = e.reshape(len(e), 1)  
             
-            rms = np.sqrt(conv2(shaped_e, shaped_h, "same"))  # 与shaped_h卷积，输出和shaped_e一样的维度，再开根号
-            epsi = 1e-10*max(rms)  # 10的-10次方乘最大值
+            rms = np.sqrt(conv2(shaped_e, shaped_h, "same")) 
+            epsi = 1e-10*max(rms)  
             np.seterr(divide='ignore', invalid='ignore')
             op = rms/(rms**2+epsi)
-            op = op.reshape(len(op),)  # 转成向量
+            op = op.reshape(len(op),)  
 
-            dout[:, k] = data[:, k]*op  # 数据的每一道乘op
+            dout[:, k] = data[:, k]*op  
 
     # Normalize by amplitude
     if option2 == 1:
-        for k in range(nx):  # 遍历每一道
+        for k in range(nx): 
             aux = dout[:, k]
             amax = max(abs(aux))
             dout[:, k] = dout[:, k]/amax
 
     # Normalize by rms
     if option2 == 2:
-        for k in range(nx):  # 遍历每一道
+        for k in range(nx):  
             aux = dout[:, k]
             amax = np.sqrt(sum(aux**2)/nt)
             dout[:, k] = dout[:, k]/amax
