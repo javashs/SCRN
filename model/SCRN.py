48a9fd198674b7228dba265f978cb6c9e7bf3d7e
javashs <91184827+javashs@users.noreply.github.com>
2023-11-14 19:21:56 +0800

    Update SCRN.py

--- model/SCRN.py
+++ model/SCRN.py
@@ -40,13 +40,13 @@ class WMSA(nn.Module):
 
     def forward(self, x):
         if self.type!='W': x = torch.roll(x, shifts=(-(self.window_size//2), -(self.window_size//2)), dims=(1,2))
-        x = rearrange(x, 'b (w1 p1) (w2 p2) c -> b w1 w2 p1 p2 c', p1=self.window_size, p2=self.window_size)
+        x = rearrange(x, 'DataSplite (w1 p1) (w2 p2) c -> DataSplite w1 w2 p1 p2 c', p1=self.window_size, p2=self.window_size)
         h_windows = x.size(1)
         w_windows = x.size(2)
 
-        x = rearrange(x, 'b w1 w2 p1 p2 c -> b (w1 w2) (p1 p2) c', p1=self.window_size, p2=self.window_size)
+        x = rearrange(x, 'DataSplite w1 w2 p1 p2 c -> DataSplite (w1 w2) (p1 p2) c', p1=self.window_size, p2=self.window_size)
         qkv = self.embedding_layer(x)
-        q, k, v = rearrange(qkv, 'b nw np (threeh c) -> threeh b nw np c', c=self.head_dim).chunk(3, dim=0)
+        q, k, v = rearrange(qkv, 'DataSplite nw np (threeh c) -> threeh DataSplite nw np c', c=self.head_dim).chunk(3, dim=0)
         sim = torch.einsum('hbwpc,hbwqc->hbwpq', q, k) * self.scale
         sim = sim + rearrange(self.relative_embedding(), 'h p q -> h 1 1 p q')
         if self.type != 'W':
@@ -55,9 +55,9 @@ class WMSA(nn.Module):
 
         probs = nn.functional.softmax(sim, dim=-1)
         output = torch.einsum('hbwij,hbwjc->hbwic', probs, v)
-        output = rearrange(output, 'h b w p c -> b w p (h c)')
+        output = rearrange(output, 'h DataSplite w p c -> DataSplite w p (h c)')
         output = self.linear(output)
-        output = rearrange(output, 'b (w1 w2) (p1 p2) c -> b (w1 p1) (w2 p2) c', w1=h_windows, p1=self.window_size)
+        output = rearrange(output, 'DataSplite (w1 w2) (p1 p2) c -> DataSplite (w1 p1) (w2 p2) c', w1=h_windows, p1=self.window_size)
 
         if self.type!='W': output = torch.roll(output, shifts=(self.window_size//2, self.window_size//2), dims=(1,2))
         return output
@@ -130,12 +130,86 @@ class ConvTransBlock(nn.Module):
     def forward(self, x):
         conv_x, trans_x = torch.split(self.conv1_1(x), (self.conv_dim, self.trans_dim), dim=1)
         conv_x = self.conv_block(conv_x) + conv_x
-        trans_x = Rearrange('b c h w -> b h w c')(trans_x)
+        trans_x = Rearrange('DataSplite c h w -> DataSplite h w c')(trans_x)
         trans_x = self.trans_block(trans_x)
-        trans_x = Rearrange('b h w c -> b c h w')(trans_x)
+        trans_x = Rearrange('DataSplite h w c -> DataSplite c h w')(trans_x)
         res = self.conv1_2(torch.cat((conv_x, trans_x), dim=1))
         x = x + res
         return x
 
 
+class SCRN(nn.Module):
+    def __init__(self, in_nc=1, config=[1,1,1,1,1], dim=64, drop_path_rate=0.0, input_resolution=128):
+        super(SCRN, self).__init__()
+        self.config = config
+        self.dim = dim
+        self.head_dim = 32
+        self.window_size = 8
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(config))]
+
+        self.m_head = [nn.Conv2d(in_nc, dim, 3, 1, 1, bias=False)]
+
+        begin = 0
+        self.m1 = [ConvTransBlock(dim//2, dim//2, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW', input_resolution)
+                      for i in range(config[0])] + \
+                      [nn.Conv2d(dim, dim, 3, 1, 1, bias=False)]
+
+        begin += config[0]
+        self.m2 = [ConvTransBlock(dim//2, dim//2, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW', input_resolution)
+                      for i in range(config[1])] + \
+                      [nn.Conv2d(dim, dim, 3, 1, 1, bias=False)]
+
+        begin += config[1]
+        self.m3 = [ConvTransBlock(dim//2, dim//2, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW',input_resolution)
+                      for i in range(config[2])] + \
+                      [nn.Conv2d(dim, dim, 3, 1, 1, bias=False)]
+
+        begin += config[2]
+        self.m4 = [ConvTransBlock(dim//2, dim//2, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW',input_resolution)
+                      for i in range(config[2])] + \
+                      [nn.Conv2d(dim, dim, 3, 1, 1, bias=False)]
+
+        begin += config[3]
+        self.m5 = [ConvTransBlock(dim//2, dim//2, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW',input_resolution)
+                      for i in range(config[2])] + \
+                     [nn.Conv2d(dim, dim, 3, 1, 1, bias=False)]
+
+        self.m_tail = [nn.Conv2d(dim, in_nc, 3,1, 1, bias=False)]
+        self.m_head = nn.Sequential(*self.m_head)
+        self.m1 = nn.Sequential(*self.m1)
+        self.m2 = nn.Sequential(*self.m2)
+        self.m3 = nn.Sequential(*self.m3)
+        self.m4 = nn.Sequential(*self.m4)
+        self.m5 = nn.Sequential(*self.m5)
+        self.m_tail = nn.Sequential(*self.m_tail)
+        self.apply(self._init_weights)
+
+    def forward(self, x0):
+        h, w = x0.size()[-2:]
+        x1 = self.m_head(x0)
+        x2 = self.m1(x1)
+        x3 = self.m2(x2)
+        x4 = self.m3(x3)
+        x5 = self.m4(x4+x3)
+        x6 = self.m5(x5+x2)
+        x7 = self.m_tail(x6+x1)
+        x7 = x7[..., :h, :w]
+        return x7
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+
+if __name__ == '__main__':
+    net = SCRN()
+    print(net)
+    x = torch.randn((1, 1, 128, 128))
+    x = net(x)
 
